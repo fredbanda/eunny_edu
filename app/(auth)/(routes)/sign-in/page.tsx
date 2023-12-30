27e7be481d6fc388b35e27b7f009df81490d2cf9
fredbanda <fredcbanda@hotmail.com>
2023-12-30 17:25:09 +0200

    added auth

---
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+export default function page() {
+  return (
+    <div>
+        <p>
+            This is unprotected page
+        </p>
+      
+    </div>
+  )
+}
--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,11 +1,10 @@
-import Image from 'next/image';
 import {Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <div>
         <p className="text-3xl font-medium w-full text-red-700">
-          Hello Eunny
+          Protected page
         </p>
         <Button variant="destructive">
             Click Me 
